11215ef41fcab5dcf3704300b780844df53ac382
LeeMinKi <118667992+Maybess@users.noreply.github.com>
2026-03-11 12:09:02 +0900

    Title: 집합의 원소, Time: 1041ms, Memory: 42MB, Status: Wrong Answer - Codetree

---
+++ 260311/집합의 원소/elements-of-a-set.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Main {
+    static int[] parent;
+    public static int find(int x)
+    {
+        if(parent[x]==x)
+        return x;
+        
+        return parent[x]=find(parent[x]);
+
+    }
+
+    public static void union(int a, int b){
+        int rootA =find(a);
+        int rootB =find(b);
+        if(rootA == rootB)
+        {
+            parent[rootB] = rootA;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        parent = new int[n+1];
+
+        for (int i = 1; i<=n; i++)
+        {
+            parent[i]=i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int qType = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            if (qType ==0)
+            {
+                union(a,b);
+            }
+            else{
+                if(find((a)) == find((b)))
+                {
+                    System.out.println(1);
+                }
+                else
+                {
+                    System.out.println(0);
+                }
+            }
+
+        }
+    }
+}
