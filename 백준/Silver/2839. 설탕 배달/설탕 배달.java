fbf8e958cfaaf7bda3acec11d1e37de9047039c7
LeeMinKi <118667992+Maybess@users.noreply.github.com>
2026-03-31 17:09:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int count = 0;
         
         while (true) {
